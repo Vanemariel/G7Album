0a13ab3ee3cc39dea0d37fcdbdb2abc535612409
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-09-20 19:35:53 -0300

    Estructura Client

--- G7Album/Client/src/App.tsx
+++ G7Album/Client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -16,10 +15,10 @@ function App() {
     probandoConexion()
   }, [])
 
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ G7Album/Client/src/Pages/Authentication/Index.tsx
@@ -0,0 +1,9 @@
+
+
+export const Authentication : React.FC = () => {
+
+
+    return (
+        <p>asd</p>
+    )
+}
