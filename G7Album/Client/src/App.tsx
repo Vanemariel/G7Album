a0de0fcb5786b34746840b7ef49de90e1ab5033d
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-09-20 18:44:08 -0300

    conexion back y front

--- G7Album/Client/src/App.tsx
+++ G7Album/Client/src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const probandoConexion = async () => {
+
+    const res = await fetch("https://localhost:7040/Api/Album/GetAll")
+
+    const data = await res.json();
+    console.log("🚀 ~ file: App.tsx ~ line 16 ~ probandoConexion ~ data", data)
+  }
+
+  useEffect(()=>{
+    probandoConexion()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
