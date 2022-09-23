f70abad96df063a5295aa4ff15d300fc2a03773e
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-09-23 16:39:17 -0300

    Datos Usuario Models Front

--- G7Album/Client/src/Models/User.models.ts
+++ G7Album/Client/src/Models/User.models.ts
@@ -1,8 +1,6 @@
 export interface UserModels {
-  Id: string;
-  UserName: string;
-  Name: string;
-  Lastname: string;
-  Email: string;
-  Token: string;
+    Id: string;
+    FullName: string;
+    Email: string;
+    Token: string;
 }
