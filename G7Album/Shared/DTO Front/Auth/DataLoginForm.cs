56fda6036a481451b38e8e8924d650c1a6c452e0
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-09-22 23:22:00 -0300

    Datos que envia el front al Back

---
+++ G7Album/Shared/DTO Front/Auth/DataLoginForm.cs
@@ -0,0 +1,9 @@
+namespace G7Album.Shared.DTO_Front {
+
+    public class DataLoginForm 
+    {
+        public string Email {get; set;}
+        public string Password {get; set;}
+    }
+
+}
---
+++ G7Album/Shared/DTO Front/Auth/DataRegisterForm.cs
@@ -0,0 +1,11 @@
+namespace G7Album.Shared.DTO_Front {
+
+    public class DataRegisterForm 
+    {
+        public string Email {get; set;}
+        public string NombreCompleto {get; set;}
+        public string Password {get; set;}
+        public string ConfirmPassowrd {get; set;}
+    }
+
+}
