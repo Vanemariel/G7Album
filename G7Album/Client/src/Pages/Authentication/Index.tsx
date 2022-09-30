ff1a0a2eee684ffa3a0c27c1cbe767c99a0940bf
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-09-30 19:05:56 -0300

    auntentication

--- G7Album/Client/src/Pages/Authentication/Index.tsx
+++ G7Album/Client/src/Pages/Authentication/Index.tsx
@@ -1,19 +1,37 @@
 import { useEffect } from "react";
+import { FormLogin } from "./Components/FormLogin/FormLogin";
+import { FormRegister } from "./Components/FormRegister/FormRegister";
+import { MessageLogin } from "./Components/MessageLogin/MessageLogin";
+import { MessageRegister } from "./Components/MessageRegister/MessageRegister";
+import { useAuth } from "./Context/useAuth";
+import AuthCSS from "./Index.module.css"
 
 
-export const Authentication : React.FC = () => {
-
-    const probandoConexion = async () => {
-
-        const res = await fetch("https://localhost:7040/Api/Album/GetAll")
+export const Authentication: React.FC = () => {
 
 
-    /// METODOS
-    useEffect(()=>{
-        probandoConexion()
-    }, [])
+    /// HOOKS
+    const storeAuth = useAuth()
+
+
 
     return (
-        <p>asd</p>
+        <main className={AuthCSS.mainAuthentication}>
+            <div className={AuthCSS.containerPage}>
+                <section className={AuthCSS["containerPage__Background"]}>
+                    <MessageLogin />
+
+                    <MessageRegister />
+                </section>
+
+                <section className={`
+                    ${AuthCSS["containerPage__Auth"]} ${AuthCSS[storeAuth.GetClassCssFormModifed()]}
+               `}>
+                    <FormLogin />
+
+                    <FormRegister />
+                </section>
+            </div>
+        </main>
     )
 }
