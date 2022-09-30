7e8423329d1f21117d9609d5b58b7f5765e31906
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-09-30 18:54:58 -0300

    Page

--- G7Album/Client/src/Pages/Authentication/Index.tsx
+++ G7Album/Client/src/Pages/Authentication/Index.tsx
@@ -1,43 +1,21 @@
 import { useEffect } from "react";
-import { FormLogin } from "./Components/FormLogin/FormLogin";
-import { FormRegister } from "./Components/FormRegister/FormRegister";
-import { MessageLogin } from "./Components/MessageLogin/MessageLogin";
-import { MessageRegister } from "./Components/MessageRegister/MessageRegister";
-import { useAuth } from "./Context/useAuth";
-import AuthCSS from "./Index.module.css"
 
 
 export const Authentication : React.FC = () => {
 
+    const probandoConexion = async () => {
 
-    /// HOOKS
-    const storeAuth = useAuth()
+        const res = await fetch("https://localhost:7040/Api/Album/GetAll")
 
-
-    /// METODOS
+        const data = await res.json();
+        console.log("🚀 ~ file: App.tsx ~ line 16 ~ probandoConexion ~ data", data)
+    }
 
     useEffect(()=>{
-        storeAuth.ChangeClassCssForm();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        probandoConexion()
     }, [])
 
     return (
-        <main className={AuthCSS.mainAuthentication}>
-            <div className={AuthCSS.containerPage}>
-                <section className={AuthCSS["containerPage__Background"]}>                
-                    <MessageLogin/>
-
-                    <MessageRegister/>
-                </section>
-
-               <section className={`
-                    ${AuthCSS["containerPage__Auth"]} ${AuthCSS[storeAuth.GetClassCssFormModifed()]}
-               `}>
-                    <FormLogin />
-                    
-                    <FormRegister />
-               </section>                                                                                                                           
-            </div>
-        </main>   
+        <p>asd</p>
     )
 }
