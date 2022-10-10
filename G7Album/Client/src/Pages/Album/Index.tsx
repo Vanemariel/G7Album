117f47f8f5fbb6f71c7336ce255b8d5eb69c68f7
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-10-10 17:23:19 -0300

    Carousel dinamico en proceso

--- G7Album/Client/src/Pages/Album/Index.tsx
+++ G7Album/Client/src/Pages/Album/Index.tsx
@@ -1,6 +1,4 @@
 import './style.css'
-import './style2.css'
-import './style3.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,12 +6,39 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AlbumesMock from './Mocks/Albumes.json'
+import { useEffect } from 'react';
+import { carouselTarjets } from '../../Utils/carouselTarjets';
 
 export const Album: React.FC = () => {
 
 
     /// METODOS
 
+    const config = [
+        {
+            individualItem: '.album-item0',
+            carouselWidth: 1000, // in px
+            carouselId: '#album-rotator0',    
+            // carousel should be <div id="carouselId3"><div id="carouselHolderId3">{items}</div></div>
+            carouselHolderId: '#album-rotator-holder0',
+        },
+        {
+            individualItem: '.album-item1',
+            carouselWidth: 1000, // in p
+            carouselId: '#album-rotator1',    
+            // carousel should be <div id="carouselId3"><div id="carouselHolderId3">{items}</div></div>
+            carouselHolderId: '#album-rotator-holder1',
+        },
+        {
+            individualItem: '.album-item2',
+            carouselWidth: 1000, // in px
+            carouselId: '#album-rotator2',    
+            // carousel should be <div id="carouselId3"><div id="carouselHolderId3">{items}</div></div>
+            carouselHolderId: '#album-rotator-holder2',
+        }
+    ]
+
+    useEffect(()=> carouselTarjets(config),[])
 
     return (
 
@@ -48,121 +73,45 @@ export const Album: React.FC = () => {
             </Container>
         </Navbar>
 
-            <div className="containerPageAlbum">
-
-                <div id="m">
-                    <div className="container">
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Escribe album o torneo deseado" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                            <div className="input-group-append">
-                                <button type="button" className="btn btn-primary">
-                                    <i className="fas fa-search"></i>
-                                </button>
-                            </div>
+        <div className="containerPageAlbum">
+            <div id="m">
+                <div className="container">
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Escribe album o torneo deseado" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                        <div className="input-group-append">
+                            <button type="button" className="btn btn-primary">
+                                <i className="fas fa-search"></i>
+                            </button>
                         </div>
                     </div>
-
-                    <br />
-
-        
-                    {
-                        AlbumesMock.map((Album: any, indexAlbum: number) => (
-
-                            <div id={`album-rotator${indexAlbum +1}`} key={indexAlbum}>
-                                <h1>{Album.titleSectionMain}</h1>
-                                <section id={`album-rotator-holder${indexAlbum +1}`}>
-                                    {
-                                        Album.targetEsports.map((eSport: any, indexEsport: number) => (
-                                            <a className={`album-item${indexAlbum +1}`} target="_top" href='#' key={indexEsport}>
-                                                <div className={`album-details${indexAlbum +1}`}>
-                                                    <span className="title"> {eSport.title}</span>
-                                                    <span className="subtext">
-                                                        {eSport.subCategorys.map((subCategory: any, indexSubCategory: number) => (
-                                                            <p key={indexSubCategory}>{subCategory.title}</p>
-                                                        ))}
-                                                    </span>
-                                                </div>
-                                            </a>
-                                        ))
-                                    }          
-                                </section>
-                            </div>
-                            
-                        ))
-                    }
-
-               
-              
-                    {/* <div id="album-rotator2">
-                        <h1>Sección Peliculas Disney</h1>
-                        <br />
-                        <div id="album-rotator-holder2">
-                            <a target="_top" className="album-item2" href="https://twitter.com/smpnjn">
-                                <span className="album-details2">
-                                    <span className="icon"><i className="far fa-at"></i> Destacado</span>
-                                    <span className="title">Monster Inc</span>
-                                    <span className="subtitle"></span>
-                                    <span className="subtext">Monster Inc<br />Monster University</span>
-                                </span>
-                            </a>
-                            <a target="_top" className="album-item2" href="https://fjolt.com/article/apple-cards-webl-gl-javascript">
-                                <span className="album-details2">
-                                    <span className="title">High school Musical</span>
-                                    <span className="subtext">High school Musical<br />High school Musical 2</span>
-                                </span>
-                            </a>
-                            <a target="_top" className="album-item2" href="https://twitter.com/smpnjn">
-                                <span className="album-details2">
-                                    <span className="title">Era del Hielo</span>
-                                    <span className="subtext">La era del hielo 1<br />La era del hielo 2<br />La era del hielo 3</span>
-                                </span>
-                            </a>
-                            <a target="_top" className="album-item2" href="https://twitter.com/smpnjn">
-                                <span className="album-details2">
-                                    <span className="title">Avatar</span>
-                                    <span className="subtext">Avatar<br />Avatar 2</span>
-                                </span>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div id="album-rotator3">
-                        <h1>Sección Anime</h1>
-                        <br />
-                        <div id="album-rotator-holder3">
-                            <a target="_top" className="album-item3" href="https://twitter.com/smpnjn">
-                                <span className="album-details3">
-                                    <span className="icon"><i className="far fa-at"></i> Destacado</span>
-                                    <span className="title">Dragon Ball</span>
-                                    <span className="subtext">Dragon Ball <br />Dragon Ball Z<br /> Dragon Ball GT</span>
-                                </span>
-                            </a>
-                            <a target="_top" className="album-item3" href="https://fjolt.com/article/apple-cards-webl-gl-javascript">
-                                <span className="album-details3">
-                                    <span className="title">Naruto</span>
-                                    <span className="subtext">Naruto<br />Naruto shippuden<br />Naruto Next Generation</span>
-                                </span>
-                            </a>
-                            <a target="_top" className="album-item3" href="https://twitter.com/smpnjn">
-                                <span className="album-details3">
-
-                                    <span className="title">Cab del Zodiaco</span>
-
-                                    <span className="subtext">Saint Seiya the Lost Canvas<br />Batalla de Poseidon<br />Batalla de Asgard</span>
-                                </span>
-                            </a>
-                            <a target="_top" className="album-item3" href="https://twitter.com/smpnjn">
-                                <span className="album-details3">
-                                    <span className="title">Ataque a los Titanes</span>
-                                    <span className="subtext">Temporada 1<br />Temporada 2</span>
-                                </span>
-                            </a>
-                        </div>
-                    </div> */}
                 </div>
-
-            </div></>
-
-        // <p>ESTO ES EL ALBUM DEL WACHO MARIAN</p>
+                <br />
+    
+                {
+                    AlbumesMock.map((Album: any, indexAlbum: number) => (
+                        <div id={`album-rotator${indexAlbum}`} key={indexAlbum}>
+                            <h1>{Album.titleSectionMain}</h1>
+                            <section id={`album-rotator-holder${indexAlbum}`}>
+                                {
+                                    Album.targetEsports.map((eSport: any, indexEsport: number) => (
+                                        <a className={`album-item${indexAlbum}`} target="_top" href='#' key={indexEsport}>
+                                            <div className={`album-details${indexAlbum}`}>
+                                                <span className="title"> {eSport.title}</span>
+                                                <span className="subtext">
+                                                    {eSport.subCategorys.map((subCategory: any, indexSubCategory: number) => (
+                                                        <p key={indexSubCategory}>{subCategory.title}</p>
+                                                    ))}
+                                                </span>
+                                            </div>
+                                        </a>
+                                    ))
+                                }          
+                            </section>
+                        </div>
+                        
+                    ))
+                }
+            </div>
+        </div></>
     );
 }
---
+++ G7Album/Client/src/Utils/carouselTarjets.js
@@ -0,0 +1,102 @@
+export const carouselTarjets = (arrayConfigs) => {
+
+    arrayConfigs.forEach(configCarousel => {
+
+        // Get items
+        const el = document.querySelector(configCarousel.individualItem);
+        const elWidth = parseFloat(
+            window.getComputedStyle(el).width) + 
+            parseFloat(window.getComputedStyle(el).marginLeft) + 
+            parseFloat(window.getComputedStyle(el).marginRight
+        );
+
+        // Track carousel
+        let mousedown = false;
+        let movement = false;
+        let initialPosition = 0;
+        let selectedItem;
+        let currentDelta = 0;
+
+        document.querySelectorAll(configCarousel.carouselId).forEach(function(item) { 
+            item.style.width = `${configCarousel.carouselWidth}px`;
+        });
+
+        document.querySelectorAll(configCarousel.carouselId).forEach(function(item) {
+            item.addEventListener('pointerdown', function(e) {
+
+                console.log("PASA X ACA AL DAR CLICK EN SECCION")
+
+                mousedown = true;
+                selectedItem = item;
+                initialPosition = e.pageX;
+                currentDelta = parseFloat(
+                    item.querySelector(
+                        configCarousel.carouselHolderId
+                    ).style.transform.split('translateX(')[1]
+                ) || 0;
+            }); 
+        });
+
+        const scrollCarousel = function(change, currentDelta, selectedItem) {
+            let numberThatFit = Math.floor(configCarousel.carouselWidth / elWidth);
+            let newDelta = currentDelta + change;
+            let elLength = selectedItem.querySelectorAll(configCarousel.individualItem).length - numberThatFit;
+            if(newDelta <= 0 && newDelta >= -elWidth * elLength) {
+                selectedItem.querySelector(configCarousel.carouselHolderId).style.transform = `translateX(${newDelta}px)`;
+            } else {
+                if(newDelta <= -elWidth * elLength) {
+                    selectedItem.querySelector(configCarousel.carouselHolderId).style.transform = `translateX(${-elWidth * elLength}px)`;
+                } else if(newDelta >= 0) {
+                    selectedItem.querySelector(configCarousel.carouselHolderId).style.transform = `translateX(0px)`;
+                }
+            }
+        }
+
+        document.body.addEventListener('pointermove', function(e) {
+            
+            // console.log("PASA POR ACA AL MOVER EL MOUSE")
+         
+            if(mousedown == true && typeof selectedItem !== "undefined") {
+                let change = -(initialPosition - e.pageX);
+                scrollCarousel(change, currentDelta, document.body);
+                document.querySelectorAll(`${configCarousel.carouselId} a`).forEach(function(item) {
+                    item.style.pointerEvents = 'none';
+                });
+                movement = true;
+            }
+        });
+
+        ['pointerup', 'mouseleave'].forEach(function(item) {
+
+            document.body.addEventListener(item, function(e) {
+                selectedItem = undefined;
+                movement = false;
+                document.querySelectorAll(`${configCarousel.carouselId} a`).forEach(function(item) {
+                    item.style.pointerEvents = 'all';
+                });
+            });
+        });
+
+        // document.querySelectorAll(configCarousel.carouselId).forEach(function(item) {
+        //     let trigger = 0;
+        //     item.addEventListener('wheel', function(e) {
+
+        //         console.log("PASA POR ACA AL ....")
+
+        //         if(trigger !== 1) {
+        //             ++trigger;
+        //         } else {
+        //             let change = e.deltaX * -3;
+        //             let currentDelta = parseFloat(item.querySelector(configCarousel.carouselHolderId).style.transform.split('translateX(')[1]) || 0;
+        //             scrollCarousel(change, currentDelta, item);
+        //             trigger = 0;
+        //         }
+        //         e.preventDefault();
+        //         e.stopImmediatePropagation();
+        //         return false;
+        //     });
+        // });
+
+    })
+
+}
