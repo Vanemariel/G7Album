373c9ed0bc8dd8da29c19943a4e11d3bc9107e77
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-10-10 17:57:50 -0300

    Carousel dinamico completo

--- G7Album/Client/src/Pages/Album/Index.tsx
+++ G7Album/Client/src/Pages/Album/Index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './style.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -16,21 +17,21 @@ export const Album: React.FC = () => {
 
     const config = [
         {
-            individualItem: '.album-item0',
+            individualItem: '#album-item0',
             carouselWidth: 1000, // in px
             carouselId: '#album-rotator0',    
             // carousel should be <div id="carouselId3"><div id="carouselHolderId3">{items}</div></div>
             carouselHolderId: '#album-rotator-holder0',
         },
         {
-            individualItem: '.album-item1',
+            individualItem: '#album-item1',
             carouselWidth: 1000, // in p
             carouselId: '#album-rotator1',    
             // carousel should be <div id="carouselId3"><div id="carouselHolderId3">{items}</div></div>
             carouselHolderId: '#album-rotator-holder1',
         },
         {
-            individualItem: '.album-item2',
+            individualItem: '#album-item2',
             carouselWidth: 1000, // in px
             carouselId: '#album-rotator2',    
             // carousel should be <div id="carouselId3"><div id="carouselHolderId3">{items}</div></div>
@@ -55,63 +56,67 @@ export const Album: React.FC = () => {
 
                             <Nav.Link href="FiguritasCompra">Figuritas</Nav.Link>
 
-                        <NavDropdown title="Mi cuenta" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="AlbumUsuario">Mis albumes</NavDropdown.Item>
-                            <NavDropdown.Item href="AlbumUsuario">Comprar albumes</NavDropdown.Item>
-                            <NavDropdown.Item href="">
-                                Cerrar sesion
-                            </NavDropdown.Item>
+                            <NavDropdown title="Mi cuenta" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="AlbumUsuario">Mis albumes</NavDropdown.Item>
+                                <NavDropdown.Item href="AlbumUsuario">Comprar albumes</NavDropdown.Item>
+                                <NavDropdown.Item href="">
+                                    Cerrar sesion
+                                </NavDropdown.Item>
+                                <input type="text" className="form-control" placeholder="Escribe album o torneo deseado" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                <div className="input-group-append">
+                                    <button type="button" className="btn btn-primary">
+                                        <i className="fas fa-search"></i>
+                                    </button>
+                                </div>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <div className="containerPageAlbum">
+                <div id="m">
+                    <div className="container">
+                        <div className="input-group mb-3">
                             <input type="text" className="form-control" placeholder="Escribe album o torneo deseado" aria-label="Recipient's username" aria-describedby="basic-addon2" />
                             <div className="input-group-append">
                                 <button type="button" className="btn btn-primary">
                                     <i className="fas fa-search"></i>
                                 </button>
                             </div>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-
-        <div className="containerPageAlbum">
-            <div id="m">
-                <div className="container">
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Escribe album o torneo deseado" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                        <div className="input-group-append">
-                            <button type="button" className="btn btn-primary">
-                                <i className="fas fa-search"></i>
-                            </button>
                         </div>
                     </div>
+                    <br />
+        
+                    {
+                        AlbumesMock.map((Album: any, indexAlbum: number) => (
+                            <div id={`album-rotator${indexAlbum}`} key={indexAlbum} className="albumRotatorContainer">
+                                <h1>{Album.titleSectionMain}</h1>
+                                <section id={`album-rotator-holder${indexAlbum}`} className="albumRotatorHolder">
+                                    {
+                                        Album.targetEsports.map((eSport: any, indexEsport: number) => (
+                                            <a  id={`album-item${indexAlbum}`}
+                                                className={`albumItem`} 
+                                                target="_top" href='#' key={indexEsport}
+                                            >
+                                                <div className={`albumItem__details`}>
+                                                    <span className="title"> {eSport.title}</span>
+                                                    <span className="subtext">
+                                                        {eSport.subCategorys.map((subCategory: any, indexSubCategory: number) => (
+                                                            <p key={indexSubCategory}>{subCategory.title}</p>
+                                                        ))}
+                                                    </span>
+                                                </div>
+                                            </a>
+                                        ))
+                                    }          
+                                </section>
+                            </div>
+                            
+                        ))
+                    }
                 </div>
-                <br />
-    
-                {
-                    AlbumesMock.map((Album: any, indexAlbum: number) => (
-                        <div id={`album-rotator${indexAlbum}`} key={indexAlbum}>
-                            <h1>{Album.titleSectionMain}</h1>
-                            <section id={`album-rotator-holder${indexAlbum}`}>
-                                {
-                                    Album.targetEsports.map((eSport: any, indexEsport: number) => (
-                                        <a className={`album-item${indexAlbum}`} target="_top" href='#' key={indexEsport}>
-                                            <div className={`album-details${indexAlbum}`}>
-                                                <span className="title"> {eSport.title}</span>
-                                                <span className="subtext">
-                                                    {eSport.subCategorys.map((subCategory: any, indexSubCategory: number) => (
-                                                        <p key={indexSubCategory}>{subCategory.title}</p>
-                                                    ))}
-                                                </span>
-                                            </div>
-                                        </a>
-                                    ))
-                                }          
-                            </section>
-                        </div>
-                        
-                    ))
-                }
             </div>
-        </div></>
+        </>
     );
 }
