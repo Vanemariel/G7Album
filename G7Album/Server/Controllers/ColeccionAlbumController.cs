4383bf78a862a6f269238fcffc46aac34b6521f7
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-10-13 20:11:58 -0300

    controller

---
+++ G7Album/Server/Controllers/ColeccionAlbumController.cs
@@ -0,0 +1,23 @@
+using G7Album.Shared.Models;
+
+namespace G7Album.Server.Controllers
+{
+    [ApiController]
+    [Route("Api/Album")]
+    public class ColecctionAlbumController : ControllerBase
+    {
+        private readonly BDContext context;
+
+        public ColecctionAlbumController(BDContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("GetAll")]
+        //metodo que me muestra la lista completa  
+        public async Task<ActionResult<List<ColecctionAlbumController>>> GetAll()
+        {
+            return await context.TablaColeccionAlbumes.Include(x => x.ListadoAlbum).ToListAsync();
+        }
+
+    }
